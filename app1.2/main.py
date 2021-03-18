249e064b1982ff43f9acf6ec50a813364fb5dc9b
sdevkota <sdevkota16@gmail.com>
2021-03-18 18:35:47 -0400

    Day 1 commit with simple example

---
+++ app1.2/main.py
@@ -0,0 +1,37 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as total amount and period of the bill
+    """
+    def __init__ (self, amount, period):
+        self.amount = amount
+        self.period = period
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat and pays the share of the bill.
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount / 2
+
+class PdfReport:
+    """
+    Creates a PDF file that contains data about the flatmates such as their names, their due amounts and the periods
+    of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatname1, flatmate2, bill):
+         pass
+
+the_bill = Bill(amount=120, period="March 2021")
+john = Flatmate(name="John", days_in_house=20)
+mary = Flatmate(name="Mary", days_in_house=25)
+
+print(john.pays(bill=the_bill))
