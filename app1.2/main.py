7cc5128b0c7baa6097783a22ef3a1dffb8cccf7e
sdevkota <sdevkota16@gmail.com>
2021-03-23 18:49:26 -0400

    day 4 fonts, image etc for pdf file

--- app1.2/main.py
+++ app1.2/main.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF
+import webbrowser
 
 class Bill:
     """
@@ -42,24 +43,32 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        # Add house icon
+        pdf.image("C:\\Users\\sdevk\\OneDrive\\Desktop\\Ardit\\app1.2\\house.png", w=30, h=30)
+
         #Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, align="C", ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt='Period:', border=1)    
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)    
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert name and due amount of first flatmate
-        pdf.cell(w=100, h=40, txt=str(flatmate1.name), border=1)    
-        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=1, ln=1)
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=str(flatmate1.name), border=0)    
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
 
         # Insert name and due amount of second flatmate
-        pdf.cell(w=100, h=40, txt=str(flatmate2.name), border=1)    
-        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=1, ln=1)
+        pdf.cell(w=100, h=25, txt=str(flatmate2.name), border=0)    
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
         
         # self.filename becasue filename is local variable inside the __init_method and not generate method. 
-        pdf.output(self.filename)     
+        pdf.output('C:\\Users\\sdevk\\OneDrive\\Desktop\\Ardit\\app1.2\\Report1.pdf', 'F')  
+
+        # To open the generated pdf automatically on a webbrowser. But doesnt seems to work (3/23/21)
+        webbrowser.open(self.filename)   
 
 the_bill = Bill(amount=120, period="April 2021")
 john = Flatmate(name="John", days_in_house=20)
